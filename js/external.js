edec735b6d390353ee8ddf57aec099965e4a23d9
samirbaniya <samirbaniya500@gmail.com>
2022-08-02 11:56:00 +0545

    for loop switch

--- js/external.js
+++ js/external.js
@@ -4,6 +4,7 @@
 
 				let car=["Toyota","Bmw","lamborgini","Ferari"];
 				console.log(car);
+				console.log(car[3]);
 
 				console.log(car.includes("Bmw"))
 
@@ -54,3 +55,50 @@
 					+"<br>weight is:"+person.weight;
 
 				}
+function displayMessage(){
+	var age=document.getElementById("age").value;
+	if(age>18){
+console.log(age)
+var message="Elligible";
+	}else{
+var message="Not Elligible";
+	}
+	alert(message);
+
+	document.getElementById("message").innerHTML=message;
+}
+
+let day;
+//console.log(new Date().getDay())
+switch(new Date().getDay()){
+	case 0:
+	day="sunday";
+	break;
+	case 1:
+	day="Monday";
+	break;
+	case 2:
+	day="Tuesday";
+	break;
+	case 3:
+	day="wednesday";
+	break;
+	case 4:
+	day="Thursday";
+	break;
+	case 5:
+	day="Friday";
+	break;
+	case 6:
+	day="Saturday";
+}
+alert("Today is"+day);
+document.getElementById("day").innerHTML="Today is"+day;
+console.log(day);
+
+function displayNameFiveTimes(){
+	for(var i=1;i<=5;i++)
+	{
+		alert("Loop"+i);
+	}
+}
